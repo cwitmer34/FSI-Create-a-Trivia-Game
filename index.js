c718482504b5e1e1e44af54bb947d823c57b7800
cwitmer34 <54679947+cwitmer34@users.noreply.github.com>
2022-04-29 13:51:36 -0400

    Created Trivia, and implemented while loop

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Your Code Here
+function playGame() {
+  
+  let userName = window.prompt(`Please enter your name in the prompt below.`)
+  let amountCorrect = 0
+
+  for (let i = 0; i < questions.length; i++) {
+    let question = questions[i]
+    let userAnswer = window.prompt(question.text)
+
+    if (userAnswer === question.correctAnswer) {
+      console.log('Correct')
+      amountCorrect++
+      console.log(amountCorrect)
+    }
+  }
+
+  window.alert(`You got ${amountCorrect} correct.`)
+
+}
+
+let playAgain = true
+
+while (playAgain === true) {
+  playGame()
+  let userChoice = window.prompt('Would you like to play the game again? Answer yes or no.')
+  if(userChoice === 'yes'){
+          playAgain = true
+      } else {
+          playAgain = false
+          window.alert('Thanks for playing the game!')
+      }
+}
